8013a4cef9dd53b9a9f6c787ed3033e604f37d0e
Grigoryower <140705935+Grigoryower@users.noreply.github.com>
2023-08-10 11:40:38 +0300

    work переделал

--- src/Main.java
+++ src/Main.java
@@ -37,10 +37,11 @@ public class Main {
     public static void task3() {
         System.out.println("Задание3");
         int speed = 60;
-        if (speed < 60) {
+        int maxspeed = 65;
+        if (speed > maxspeed) {
             System.out.println("Если скорость больше " + speed + " , то придется заплатить штраф ");
         }
-        if (speed <= speed) {
+        if (speed <= maxspeed) {
             System.out.println("Если скорость " + speed + " , то   можно ездить спокойно");
         }
     }
@@ -53,22 +54,23 @@ public class Main {
         int age3 = 18;
         int age4 = 24;
         int age5 = 60;
-        if (age <= age) {
+        int x = 11;
+        if (age <= x) {
             System.out.println("если человеку меньше " + age + " лет, то ему пора спать");
         }
         if (age == 2 && age1 == 6) {
             System.out.println("Если человеку от 2 до 6 лет, то ему нужно ходить в детский сад.");
         }
-        if (age2 >= 7 || age3 == 18) {
+        if (age2 >= 7 && age3 == 18) {
             System.out.println("Если человеку от 7 до 18 лет, то ему нужно ходить в школу.");
         }
-        if (age3 >= 18 || 23 <age4) {
+        if (age3 >= 19 && x <age4) {
             System.out.println("Если человеку больше 18 лет, но меньше 24, то его место в университете");
         }
-        if (age4 >= age4) {
+        if (age4 > x) {
             System.out.println("А если человеку больше 24, то ему пора ходить на работу.");
         }
-        if (age5 >= 60) {
+        if (age5 >= x) {
             System.out.println("А если человеку больше 60, то он может отдохнуть.");
         }
 
@@ -77,16 +79,15 @@ public class Main {
 
     public static void task5() {
         System.out.println("Задание5");
-        int age = 9;
-        int age1 = 14;
-        if (age < 5) {
-            System.out.printf("Если возраст ребенка меньше " + age +" нельзя кататься на аттракционе");
+        int x = 12;
+        if (x < 5) {
+            System.out.printf("Если возраст ребенка меньше " + x +" нельзя кататься на аттракционе\n");
         }
-        else if (age > 5 && age  < age1){
-            System.out.printf(" Если ребенку ровно " + age + " лет или больше, но меньше "+ age1 + " лет, то он может кататься только в сопровождении взрослого");
+        else if (x > 5 && x  < 14){
+            System.out.printf(" Если ребенку ровно " + x + " лет или больше, но меньше 14 лет, то он может кататься только в сопровождении взрослого\n");
         }
         else  {
-            System.out.println("Если возраст ребенка равен "+ age1 + " то он может кататься без сопровождения взрослого.");
+            System.out.println("Если возраст ребенка равен "+ x + " то он может кататься без сопровождения взрослого.\n");
         }
 
     }
@@ -96,12 +97,13 @@ public class Main {
      int places= 60;
      int standing = trainCar - places;
      int peremen = 60;
-     if (places < 60 ){
-        System.out.printf("В вагоне есть места"+ (peremen-places)+ " сидяч мест");
+     int standingB= 26;
+     if (places < peremen ){
+        System.out.printf("В вагоне есть места"+ (places-standingB)+ " сидяч мест");
      } else if (places < standing){
-        System.out.printf("стоячих мест");
+        System.out.printf("В вагоне все еще есть"+ (places - standingB)+ "стоячих мест\n");
      } else  {
-            System.out.printf("вагон полностью забит");
+            System.out.printf("вагон полностью забит\n");
         }
     }
      public static void task7() {
@@ -109,13 +111,12 @@ public class Main {
         int one=1;
         int two=2;
         int three=3;
-        if (one > two || one > three) {
+        if (one > two && one > three) {
             System.out.printf("One");
         }
-        else if (two > three) {
+        else if (two > three && two > one) {
             System.out.printf("two");
-        } else {
+        } else if (three > two && three > one)
             System.out.printf("three");
         }
     }
-}
